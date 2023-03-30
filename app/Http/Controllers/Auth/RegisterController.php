2a2c84ce80b74278a9db42b7b96b934ea56e8af0
Dannis Cheang <m_sv@kaikaisd.link>
2023-03-31 02:36:58 +0800

    fk

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -75,7 +75,7 @@ class RegisterController extends Controller
             'name' => $data['name'],
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
-            'address' => $data['address1'] . "," . $data['address2'] . "," . $data['city'] . "," . $data['state'] . "," . $data['zip'],
+            'address' => $data['address1'] . ", " . $data['address2'] . "," . $data['city'] . ", " . $data['state'] . ", " . $data['zip'], 
         ]);
     }
 }
