f6b0eda1f531954f6048ecddd4a9cd144e2191b5
Dannis Cheang <m_sv@kaikaisd.link>
2023-04-14 13:50:38 +0800

    Bug fix

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -181,7 +181,7 @@ class OrderController extends Controller
             return redirect()->route('order.review', $request->order_id)->with('error', 'You are not allowed to review this product');
         }
         foreach($request->product_id as $value){
-            $comment = (Comment::find($value) ?? new Comment());
+            $comment = (Comment::where('product_id',$value) ?? new Comment());
             $comment->user_id = auth()->user()->id;
             $comment->order_id = $request->order_id;
             $comment->product_id = $value;
